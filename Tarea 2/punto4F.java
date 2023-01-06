f51f5561828e0aa44ee5ff542a85c0f39a4424af
papinautico <46242167+papinautico@users.noreply.github.com>
2023-01-06 17:24:23 +0100

    Add files via upload

---
+++ Tarea 2/punto4F.java
@@ -0,0 +1,7 @@
+public class punto4F {
+    public static void main(String[] args) {
+        for (int numerofor = 0; numerofor <= 3; numerofor = numerofor + 1) {
+            System.out.println(numerofor);
+        }
+    }
+}
