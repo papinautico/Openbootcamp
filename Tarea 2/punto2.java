0cfc2b11b8bc0792760dd37e644f121c5489d778
papinautico <46242167+papinautico@users.noreply.github.com>
2023-01-06 16:58:39 +0100

    Punto 2 "While"

---
+++ Tarea 2/punto2.java
@@ -0,0 +1,10 @@
+public class punto2 {
+    public static void main(String[] args) {
+        int numero = 0;
+        while (numero < 3) {
+            System.out.println(numero);
+            numero = numero + 1;
+        }
+
+    }
+}
