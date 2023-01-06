d40d7deb796dd4e931a28f52c6a08ab35facd652
papinautico <46242167+papinautico@users.noreply.github.com>
2023-01-06 14:54:13 +0100

    Create tareasi.java

---
+++ Tarea 2/tareasi.java
@@ -0,0 +1,15 @@
+public class tareasi {
+    public static void main(String[] args) {
+        int numero = 8;
+        if (numero == 0) {
+            System.out.println("La variable es 0.");
+        } else {
+            if (numero > 0) {
+                System.out.println("La variable es Positiva.");
+            }
+            if (numero < 0) {
+                System.out.println("La variable es Negativa.");
+            }
+        }
+    }
+}
