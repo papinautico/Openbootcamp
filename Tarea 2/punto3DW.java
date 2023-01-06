f1849b37004d7f05e68f00b1bed33296a3cc3e11
papinautico <46242167+papinautico@users.noreply.github.com>
2023-01-06 17:11:41 +0100

    Add files via upload

---
+++ Tarea 2/punto3DW.java
@@ -0,0 +1,10 @@
+public class punto3DW {
+    public static void main(String[] args) {
+        int numero = 3;
+        do {
+            System.out.println(numero);
+            numero = numero + 1;
+        } while (numero < 3);
+    }
+
+}
